2bcae4f441d7001603431ee4de177db46ef7b96c
y_aut <y_autumn0901@icloud.com>
2023-09-15 13:13:55 +0900

    Initial Commit

---
+++ RegEvent/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RegEvent
+//
+//  Created by 山下暁天 on 2023/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RegEvent/RegEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegEventApp.swift
+//  RegEvent
+//
+//  Created by 山下暁天 on 2023/09/15.
+//
+
+import SwiftUI
+
+@main
+struct RegEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
